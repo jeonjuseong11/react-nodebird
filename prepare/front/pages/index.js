022db892479ccd94c5e837a4843825d37fee328c
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-03-27 23:03:33 +0900

    page & layout
    React로 NodeBird SNS 만들기

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,4 +1,10 @@
+import AppLayout from "../components/AppLayout";
+
 const Home = () => {
-  return <div>Hello, Next</div>;
+  return (
+    <AppLayout>
+      <div>Hello, Next</div>
+    </AppLayout>
+  );
 };
 export default Home;
