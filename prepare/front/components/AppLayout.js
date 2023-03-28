919481d5e48fb2c166b5e25a17ac91ae92ab4075
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-03-28 22:51:35 +0900

    Feat : Login
    프론트에서 로그인 구현

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,62 @@
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
+import { Col, Input, Menu, Row } from "antd";
+
+import UserProfile from "../components/UserProfile";
+import LoginForm from "../components/LoginForm";
+
+import styled from "styled-components";
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
+
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-      <div>공통메뉴</div>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <SearchInput enterButton />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://juseongsdevstory.tistory.com/"
+            target="_blank"
+            rel="noreferer noopener"
+          >
+            Made by Jeo
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
---
+++ prepare/front/components/LoginForm.js
@@ -0,0 +1,60 @@
+import { Button, Form, Input } from "antd";
+import React, { useState, useCallback, useMemo } from "react";
+import Link from "next/link";
+import styled from "styled-components";
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
+const LoginForm = ({ setIsLoggedIn }) => {
+  const style = useMemo(
+    () => ({
+      marginTop: 10,
+    }),
+    []
+  );
+
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+  const onSubmitForm = useCallback(() => {
+    console.log(id, password);
+    setIsLoggedIn(true);
+  }, [id, password]);
+  return (
+    <FormWrapper onFinish={onSubmitForm}>
+      <div>
+        <label htmlFor="user-id">id</label>
+        <br />
+        <Input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-pw">pw</label>
+        <br />
+        <Input
+          name="user-pw"
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      <div style={style}>
+        <Button type="primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </div>
+    </FormWrapper>
+  );
+};
+export default LoginForm;
